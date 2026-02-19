6616b2ebd9bba66af1de279828dd2591078feb78
muhammadlazz <muhlazuuardi@gmail.com>
2026-02-20 00:02:03 +0700

    fix prisma config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,25 +1,3 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
 
-const rawConnectionString = process.env.DATABASE_URL!
-const url = new URL(rawConnectionString)
-const sslmode = url.searchParams.get('sslmode')
-
-if (
-  (sslmode === 'prefer' || sslmode === 'require' || sslmode === 'verify-ca') &&
-  !url.searchParams.has('uselibpqcompat')
-) {
-  url.searchParams.set('uselibpqcompat', 'true')
-}
-
-const connectionString = url.toString()
-const sslRequired = sslmode === 'require'
-
-const pool = new pg.Pool({
-  connectionString,
-  ...(sslRequired ? { ssl: { rejectUnauthorized: false } } : {}),
-})
-const adapter = new PrismaPg(pool)
-
-export const prisma = new PrismaClient({ adapter }) // v7 now requires the adapter here
+export const prisma = new PrismaClient()
